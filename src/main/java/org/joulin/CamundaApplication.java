56e0dda93c640566b86b6193903ec973da21253d
arlet <shulga@niuitmo.ru>
2024-05-31 03:44:25 +0300

    initial commit

---
+++ src/main/java/org/joulin/CamundaApplication.java
@@ -0,0 +1,26 @@
+package org.joulin;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@EnableProcessApplication
+@SpringBootApplication
+public class CamundaApplication {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("app");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaApplication.class, args);
+    }
+}
